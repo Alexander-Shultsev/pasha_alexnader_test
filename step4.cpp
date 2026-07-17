c4b3d083acf1395d88d5b3a193cb4231c7537348
swzxc <kirillminin40047@gmail.com>
2026-07-17 15:57:56 +0300

    че то сделал сам не понял

--- step4.cpp
+++ step4.cpp
@@ -19,31 +19,38 @@ int main() {
 
     // Мы просим память под (pointNum * 4 байта) и приводим результат к указателю (int32_t*)
     int32_t* altitudes = (int32_t*)malloc(pointNum * sizeof(int32_t));
-    int32_t* longtitude = (int32_t*)malloc(pointNum * sizeof(int32_t));
+    int32_t* longtitudes = (int32_t*)malloc(pointNum * sizeof(int32_t));
+    int32_t* latitudes = (int32_t*)malloc(pointNum * sizeof(int32_t));
 
     // 2. ПРОВЕРКА ОБЕИХ ОБЛАСТЕЙ ПАМЯТИ (Защита от сбоя)
-    if (altitudes == NULL || longtitudes == NULL) {
+    if (altitudes == NULL || longtitudes == NULL || latitudes == NULL) {
         cout << "Критическая ошибка: не удалось выделить память под миссию!" << endl;
         // Если первый успел выделиться, а второй нет - нужно очистить первый перед выходом!
         if (altitudes != NULL) free(altitudes);
         if (longtitudes != NULL) free(longtitudes);
+        if (latitudes != NULL) free(latitudes);
         return -1;
     }
 
     // 2. РАБОТА С МАССИВОМ (заполняем высоты с шагом 50 метров)
     for (int i = 0; i < pointNum; i++) {
         altitudes[i] = (i + 1) * 50; // Точка 1: 50м, Точка 2: 100м и т.д.
+        latitudes[i] = (i + 1) * 50;
     }
 
     // Выводим сгенерированную миссию на экран
     cout << "\n--- Сгенерированный профиль высот ---" << endl;
     for (int i = 0; i < pointNum; i++) {
-        cout << "Точка #" << (i + 1) << altitudes[i] << << endl;
+        cout << "Точка #" << (i + 1) << altitudes[i] << endl;
+        cout << "Точка #" << (i + 1) << latitudes[i] << endl;
     }
 
     // 3. ОСВОБОЖДЕНИЕ ПАМЯТИ (Обязательно!)
     free(altitudes);
     altitudes = NULL; // Хороший тон: обнулить указатель после освобождения
+
+    free(latitudes);
+    latitudes = NULL;
 
     cout << "\nПамять успешно очищена. Миссия завершена." << endl;
     return 0;
