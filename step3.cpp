3d441faeaff13d9a396e01421be6ddef3e46a281
swzxc <kirillminin40047@gmail.com>
2026-07-17 15:40:16 +0300

    увеличение высоты

--- step3.cpp
+++ step3.cpp
@@ -10,16 +10,31 @@ void testReference(int32_t& f) {
     f -= 30;
 }
 
+void testAltitude(int32_t f) {
+    f += 25;
+}
+
+void testRefAlti(int32_t& f) {
+    f += 25;
+}
+
 int function() {
     using namespace std;
 
     int32_t fuel = 100;
+    int32_t altitude = 60;
 
     testValue(fuel);
     cout << "fuel - " << fuel << endl;
 
     testReference(fuel);
     cout << "fuel - " << fuel << endl;
+
+    testAltitude(altitude);
+    cout << "altitude - " << altitude << endl;
+
+    testRefAlti(altitude);
+    cout << "altitude - " << altitude << endl;
 
 
     return 0;
